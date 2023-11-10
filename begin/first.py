20737e58892308d6c5e46ba16997790358d19e03
TeddyGavi <matc.davis@gmail.com>
2023-11-09 23:08:28 -0800

    different syntax!

---
+++ begin/first.py
@@ -0,0 +1,7 @@
+user_input = input("Enter Something Please? ").strip().title()
+print(f"Hello, World, this was your input: {user_input}")
+
+x1 = input("What is x? ")
+y1 = input("What is y? ")
+
+print("The sum is:", int(x1) + int(y1))
