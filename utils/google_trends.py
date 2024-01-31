e9e1538454e5ebb7ed2ebb12f28db40eb852eb81
TeddyGavi <matc.davis@gmail.com>
2024-01-30 22:42:15 -0800

    more play, shame wizard approaches

--- utils/google_trends.py
+++ utils/google_trends.py
@@ -6,3 +6,10 @@ pytrend = TrendReq()
 trending = pytrend.today_searches(pn="US")
 results = pd.DataFrame(trending).head(20)
 print(results.to_string())
+
+df = pd.DataFrame(trending.split(""))
+
+df["Query"] = df["Query"].str.extract(r"q=(*.?)&")
+df["date"] = df["date"].str.extract(r"date=(*.?)&")
+
+print(df)
