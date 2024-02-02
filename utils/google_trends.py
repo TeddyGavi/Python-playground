06422e1832ecc95d57184e799de89beb6083ffa3
TeddyGavi <matc.davis@gmail.com>
2024-02-01 21:42:54 -0800

    more testing with pytrends...

--- utils/google_trends.py
+++ utils/google_trends.py
@@ -3,13 +3,25 @@ from pytrends.request import TrendReq
 
 pytrend = TrendReq()
 
-trending = pytrend.today_searches(pn="US")
-results = pd.DataFrame(trending).head(20)
-print(results.to_string())
+trending = pytrend.today_searches(pn="CA")
+# df = pd.DataFrame(trending)
+#
+# # print(results.to_string())
+#
+# # df = pd.DataFrame(trending.split(""))
+# #
+# # df["Query"] = df["Query"].str.extract(r"q=(*.?)&")
+# # df["date"] = df["date"].str.extract(r"date=(*.?)&")
+# #
+# print(df)
+#
+# df["exploreLink"] = df["exploreLink"].str.extract(r"q=(.*?)&")
+# print(df)
+#
+# print(df.head(20))  # Display DataFrame
+# Replace '+' with space in each query
+trending = [query.replace("+", " ") for query in trending]
 
-df = pd.DataFrame(trending.split(""))
-
-df["Query"] = df["Query"].str.extract(r"q=(*.?)&")
-df["date"] = df["date"].str.extract(r"date=(*.?)&")
-
-print(df)
+# Create DataFrame with 'exploreLink' column
+df = pd.Series(trending)
+print(df.values, df.shape)
