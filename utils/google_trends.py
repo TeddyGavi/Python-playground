4ce309a87d27aba16ad073cf68b5afbbdcc3eb89
TeddyGavi <matc.davis@gmail.com>
2024-02-20 21:55:39 -0800

    more testing

--- utils/google_trends.py
+++ utils/google_trends.py
@@ -4,24 +4,23 @@ from pytrends.request import TrendReq
 pytrend = TrendReq()
 
 trending = pytrend.today_searches(pn="CA")
-# df = pd.DataFrame(trending)
+print(trending)
+df = pd.DataFrame(trending)
+# print(results.to_string())
 #
-# # print(results.to_string())
+
+# df = pd.DataFrame(trending.split(""))
 #
-# # df = pd.DataFrame(trending.split(""))
-# #
-# # df["Query"] = df["Query"].str.extract(r"q=(*.?)&")
-# # df["date"] = df["date"].str.extract(r"date=(*.?)&")
-# #
-# print(df)
+# df["Query"] = df["Query"].str.extract(r"q=(*.?)&")
+# df["date"] = df["date"].str.extract(r"date=(*.?)&")
 #
-# df["exploreLink"] = df["exploreLink"].str.extract(r"q=(.*?)&")
-# print(df)
-#
-# print(df.head(20))  # Display DataFrame
-# Replace '+' with space in each query
+print(df)
+
+df["exploreLink"] = df["exploreLink"].str.extract(r"q=(.*?)&")
+print(df)
+
+print(df.head(20))  # Display DataFrame
 trending = [query.replace("+", " ") for query in trending]
 
 # Create DataFrame with 'exploreLink' column
-df = pd.Series(trending)
-print(df.values, df.shape)
+# df = pd.Series(trending)
